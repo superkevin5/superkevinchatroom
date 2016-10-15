5a721fdb2db446c32cad7b53feba6e82128278cf
superkevin5 <hlmloveaustralia@gmail.com>
2016-10-15 22:51:04 +1100

    fisrst commmit

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+
+server.listen(process.env.PORT || 3000);
+console.log('Server Running...');
+
+var users = [];
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+	console.log('Socket Connected...');
+
+	// Send Message
+	socket.on('send message', function(data){
+		io.sockets.emit('new message', {msg: data,user:socket.username});
+	});
+
+
+	socket.on('userlogin',function(data){
+		socket.username = data;
+		users.push(data);
+		console.log(data);
+		io.sockets.emit('numberOfusers', users);
+	});
+
+
+	socket.on('disconnect',function(data){
+
+
+		if(!socket.username){
+			return;
+		}
+		console.log('Socket '+socket.username + ' Connected...');
+		users.splice(users.indexOf(socket.username), 1);		
+	});
+
+
+});
